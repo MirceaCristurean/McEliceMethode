892ce2e618bde93d1553c3a8cfb1a89897424eaf
Cristurean, Mircea <m.cristurean@krohne.com>
2023-01-26 18:09:24 +0200

    updated matrixC

--- matrix.py
+++ matrix.py
@@ -16,7 +16,6 @@ def randomMatrix(rows, columns):
 
 
 def randomVector(length, w):
-    #rand = randrange(0, length+1)
     arr = [True]*w+[False]*(length-w)
     shuffle(arr)
     return np.array(arr, dtype='bool')
@@ -55,54 +54,32 @@ n = 30  # coloane
 k = 20   # n-k randuris
 w = 5  # numar de 1 pe linie
 
-
-#Su = np.random.default_rng().choice(n, size=w, replace=False)
-# vector de n zeros cu index de Su = 1
-Su = np.zeros(n)
-for index in np.random.default_rng().choice(n, size=w, replace=False):
-    Su[index] = 1
-Sv = np.zeros(n)
-for index in np.random.default_rng().choice(n, size=w, replace=False):
-    Sv[index] = 1
-Pu = np.poly1d(Su)
-Pv = np.poly1d(Sv)
-
-oneX = np.poly1d([1, 0])  # 1x
-
-#H = np.concatenate((PuSide, PvSide), axis=1)
-identity = np.identity(n, dtype='bool')
-
 H = matrixH(n-k, n, w)
-# print_matrix(H)
-print_rank(H)
-print("Number of rows ", len(H))
 
-H_final = np.copy(H)
-delta = n-k-rank(H)
-if delta != 0:
-    print('Rank deficient by ', delta)
-    H_temp = np.copy(H)
-    while(delta > 0):
-        for column in range(0, len(H_temp)):
-            H_temp = np.delete(H_temp, column, 0)
-            if(rank(H) == rank(H_temp)):
-                H = np.copy(H_temp)
-                H_final = np.copy(H_temp)
+
+def remove_duplicates_rows(H):
+    H_final = np.copy(H)
+    delta = n-k-rank(H)
+    if delta != 0:
+        H_temp = np.copy(H)
+        while (delta > 0):
+            for column in range(0, len(H_temp)):
+                H_temp = np.delete(H_temp, column, 0)
+                if (rank(H) == rank(H_temp)):
+                    H = np.copy(H_temp)
+                    H_final = np.copy(H_temp)
                 break
             else:
                 H_temp = np.copy(H)
         delta = delta - 1
-else:
-    print('Rank full')
-
-H = H_final
-# print_matrix(H_final)
-print_rank(H_final)
-lenH = len(H)
-print("Number of rows ", lenH)
+    else:
+        print('Rank full')
+    return H_final
 
 
-# alta metoda while rank < m-k, vertical join (H, shuffle 1*w 0*n-m)
+H = remove_duplicates_rows(H)
+
+
 def calcul_Lscor(H, S):
     LS, = S.nonzero()
     Lscor = []
@@ -113,29 +90,18 @@ def calcul_Lscor(H, S):
     return Lscor
 
 
-#B = Matrix((H*1).astype(bool)).rref(pivots=False)
-print(H*1)
-
-
 # Reduced Row Echelon Form
 def rref(A):
     m, n = A.shape
-    #k = m - n
     i, j = 0, 0
     jb = []
     LCS = []  # permutarea
-
     while i < m and j < n:
         # Find value and index of largest element in the remainder of column j
         k = np.argmax(np.abs(A[i:m, j])) + i
-        print('k ', k)
         p = np.abs(A[k, j])
-        print('p ', p*1)
-        print('i ', i)
         if p*1 < 1:
             # The column is negligible, zero it out
-            # A[i:m, j] = 0.0
-            print('j', j)
             LCS.append(j)
             j = j + 1
         else:
@@ -144,80 +110,51 @@ def rref(A):
             if i != k:
                 # Swap the i-th and k-th rows
                 A[[i, k], j:n] = A[[k, i], j:n]
-                print("swap", i, k)
-                print_matrix(A*1)
-            # Divide the pivot row i by the pivot element A[i, j]
-            # A[i, j:n] = A[i, j:n] / A[i, j]
             # Subtract multiples of the pivot row from all the other rows
             for l in range(i+1, m):
                 if A[l, j] == 1:
                     A[l, j:n] = np.logical_xor(A[l, j:n], A[i, j:n])
-
             i += 1
             j += 1
-            print('i', i, 'j', j)
-            print()
-    print('LCS', LCS)
-    print_matrix(A*1)
     for index in LCS:
         A = np.concatenate((A, np.vstack(A[:, index])), axis=1)
-    print('After Concat')
-    print_matrix(A*1)
     for index in reversed(LCS):
         A = np.delete(A, index, 1)
-    print('After Delete')
-
     for j in range(m-1, 0, -1):
         L, = np.nonzero(A[0:j, j])
         for p in L:
             A[p, :] = np.logical_xor(A[j, :], A[p, :])
         np.identity(n-m)
         print(L)
-
-    # Finished
-    print_matrix(A*1)
     return A, jb
 
 
 A, jb = rref(np.copy(H))
-
-# S un vector de eroare de lungime n-k
-#S = randomVector(lenH)
 e = randomVector(n, 3)
-# print(H*1)
-# print('e', e*1)
-
 S = H @ e
 sindrome = np.copy(S)
-# print('S', S*1)
-
-# y = np.zeros(n, dtype="bool")
-# length, = S.nonzero()
-# while len(length) != 0:
-#     scor = calcul_Lscor(H, S)
-#     print('scor', scor)
-#     M = max(scor)
-#     print('M', M)
-#     ListMax, = (np.array(scor) >= M).nonzero()
-#     print('ListaMax', ListMax)
-#     V = np.zeros(n-k, dtype="bool")
-#     x = np.zeros(n, dtype="bool")
-#     for column in ListMax:
-#         #print(np.transpose(H)[:][column] * 1)
-#         V = V + np.transpose(H)[:][column]
-#         x[column] = 1
-#     #print('V', V)
-#     S = np.logical_xor(S, V)
-#     y = y + x
-
-#     length, = S.nonzero()
-#     if time() > timeout:
-#         break  # the loop after {timeout} amount of seconds
-# print('e', e.nonzero())
-# print('y', y.nonzero())
-# print((H@y)*1)
-# print('Sindrome', sindrome*1)
-# print(np.equal(H@y, sindrome)*1)
+y = np.zeros(n, dtype="bool")
+length, = S.nonzero()
+while len(length) != 0:
+    scor = calcul_Lscor(H, S)
+    M = max(scor)
+    ListMax, = (np.array(scor) >= M).nonzero()
+    V = np.zeros(n-k, dtype="bool")
+    x = np.zeros(n, dtype="bool")
+    for column in ListMax:
+        V = V + np.transpose(H)[:][column]
+        x[column] = 1
+    S = np.logical_xor(S, V)
+    y = y + x
+    length, = S.nonzero()
+    if time() > timeout:
+        break  # the loop after {timeout} amount of seconds
+print("Finish")
+print('e', e.nonzero())
+print('y', y.nonzero())
+print((H@y)*1)
+print('Sindrome', sindrome*1)
+print(np.equal(H@y, sindrome)*1)
 
 
 # LS egal index in S fara 0         LS = index(1's in S)
